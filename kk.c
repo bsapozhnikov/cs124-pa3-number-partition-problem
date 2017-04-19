8fbd1c42ee3f1a6e383a26176cf701d5f34d27d2
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2017-04-19 16:59:30 -0400

    made python executable

---
+++ kk.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Usage: ./kk inputfile");
+    return 1;
+  }
+  char *py_argv[3];
+  py_argv[0] = "python";
+  py_argv[1] = "./kk.py";
+  py_argv[2] = argv[1];
+  execvp("python", py_argv);
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+kk: kk.c
+	gcc -o ./kk kk.c
