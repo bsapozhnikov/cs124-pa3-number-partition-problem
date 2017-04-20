bb9794c3bbcc22a306dcb28aa314f93e5ea42b8d
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2017-04-20 15:28:27 -0400

    finalized code?!

--- kk.c
+++ kk.c
@@ -2,14 +2,10 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    printf("Usage: ./kk inputfile\n");
-    return 1;
-  }
   char *py_argv[4];
   py_argv[0] = "python";
   py_argv[1] = "./kk.py";
-  py_argv[2] = argv[1];
+  py_argv[2] = argc > 1 ? argv[1] : NULL;
   py_argv[3] = NULL;
   execvp("python", py_argv);
 }
--- kk.py
+++ kk.py
@@ -1,18 +1,68 @@
 from sys import argv, exit, maxint
+from time import time
 import random, math
 
 INPUT_LEN = 100
-NUM_TRIALS = 25000
+MAX_VAL = 10 ** 12
+NUM_ITERS = 25000
+NUM_TRIALS = 100
 
-def getArray(filename="ones.txt"):
+####################
+###   HELPERS    ###
+####################
+
+### getArray(filename)
+###   reads array from file named `filename` and returns it
+### Invariant: `filename` is a list of INPUT_LEN integers, one per line
+def getArray(filename):
     inputfile = open(filename, 'r')
-    A = []
-    for _ in xrange(INPUT_LEN):
-        A.append(int(inputfile.readline()))
-    return A
+    return [int(inputfile.readline()) for _ in xrange(INPUT_LEN)]
 
-### Insert x into A     ###
-### Assumes A is sorted ###
+### genRandArray()
+###   generates and returns random array of INPUT_LEN values,
+###   each between 0 and MAX_VAL, inclusive
+def genRandArray():
+    return [random.randint(0, MAX_VAL) for _ in xrange(INPUT_LEN)]
+    
+### genSolutionSeq()
+###   generate and return a random solution,
+###   represented as a list of +1s and -1s
+def genSolutionSeq():
+    return [random.choice([-1, 1]) for _ in xrange(INPUT_LEN)]
+
+### genPrepartition()
+###   generate and return a random prepartition,
+###   represented as a list of random group ids
+def genPrepartition():
+    return [random.randint(0, INPUT_LEN - 1) for _ in xrange(INPUT_LEN)]
+
+### genSeqNeighbor(S)
+###   generate and return a random neighbor for a solution sequence,
+###   where moving from S to a neighbor is accomplished by swapping
+###   either one or two elements of S
+### Invariant: S is a valid solution sequence
+def genSeqNeighbor(S):
+    sample = random.sample(xrange(INPUT_LEN), 2)
+    S[sample[0]] *= -1
+    if (random.randint(0, 1)):
+        S[sample[1]] *= -1
+    return S
+
+### genPrePartNeighbor(P)
+###   generate and return a random neighbor for a prepartition,
+###   where moving from P to a neighbor is accomplished by changing
+###   which partition on element lies in
+### Invariant: P is a valid prepartition
+def genPrePartNeighbor(P):
+    sample = random.sample(xrange(INPUT_LEN), 2)
+    while (P[sample[0]] == sample[1]):
+        sample = random.sample(xrange(INPUT_LEN), 2)
+    P[sample[0]] = sample[1]
+    return P
+    
+### binsert()
+###   insert x into A in sorted position, using binary search
+### Invariant: A is sorted
 def binsert(A, x):
     i,j = 0,len(A)
     while i < j:
@@ -25,7 +75,60 @@ def binsert(A, x):
         else:
             j = k
     A.insert(i, x)
-    
+
+### prePartition(A, P)
+###   partition a list given a prepartition
+### Invariant: len(A) == INPUT_LEN; P is a valid prepartition
+def prePartition(A, P):
+    APartitioned = [0] * INPUT_LEN
+    for i in xrange(INPUT_LEN):
+        APartitioned[P[i]] += A[i]
+    return APartitioned
+
+### getResidueSeq(A, S)
+###   return residue given array A and sequence S
+### Invariants: len(A) == INPUT_LEN; A is a valid solution sequence
+def getResidueSeq(A, S):
+    return abs(sum(A[i] * S[i] for i in xrange(INPUT_LEN)))
+
+### getResiduePP(A, P)
+###   return residue given array A and partition P
+### Invariants: len(A) == INPUT_LEN; P is a valid prepartition
+def getResiduePP(A, P):
+    return kk(prePartition(A,P))
+
+### saCooling(i)
+###   cooling schedule function for simulated annealing ###
+def saCooling(i):
+    return (10**8) * (0.8**(i // 300))
+
+####################
+###  EXPERIMENT  ###
+####################
+
+def timing(f):
+    def wrap(*args):
+        start = time()
+        res = f(*args)
+        end = time()
+        return (res, end - start)
+    return wrap
+
+def experiment(f):
+    def wrap(_A):
+        resSum = timeSum = 0
+        for _ in xrange(NUM_TRIALS):
+            (res, time) = timing(f)(genRandArray())
+            resSum += res
+            timeSum += time
+        return (resSum / NUM_TRIALS, timeSum / NUM_TRIALS)
+    return wrap
+
+####################
+###  ALGORITHMS  ###
+####################
+
+### Karmarkar-Karp
 def kk(A):
     A.sort()
     for _ in xrange(len(A)-1):
@@ -34,88 +137,32 @@ def kk(A):
         binsert(A, abs(m1 - m2))
     return A[0]
 
-### Generate and return a random solution  ###
-###   represented as a list of +1s and -1s ###
-def genSolutionSeq():
-    S = []
-    for _ in xrange(INPUT_LEN):
-        S.append(random.choice([-1,1]))
-    return S
-
-### Generate and return a random prepartition   ###
-###   represented as a list of random group ids ###
-def genPrepartition():
-    P = []
-    for _ in xrange(INPUT_LEN):
-        P.append(random.randint(0,INPUT_LEN - 1))
-    return P
-
-### Partition a list given a prepartition ###
-def prePartition(A, P):
-    APartitioned = [0] * INPUT_LEN
-    for i in xrange(INPUT_LEN):
-        APartitioned[P[i]] += A[i]
-    return APartitioned
-    
-
-### Generate and return a random       ###
-###   neighbor for a solution sequence ###
-def genSeqNeighbor(S):
-    sample = random.sample(xrange(INPUT_LEN), 2)
-    S[sample[0]] *= -1
-    if (random.randint(0, 1)):
-        S[sample[1]] *= -1
-    return S
-
-### Generate and return a random  ###
-###   neighbor for a prepartition ###
-def genPrePartNeighbor(P):
-    sample = random.sample(xrange(INPUT_LEN), 2)
-    while (P[sample[0]] == sample[1]):
-        sample = random.sample(xrange(INPUT_LEN), 2)
-    P[sample[0]] = sample[1]
-    return P
-    
-### Return residue given array A and sequence S ###
-###   A[i] is in partition "1" iff S[i] == 1    ###
-###   else A[i] is in partition "-1"            ###
-### Invariants:                                 ###
-###   S[i] == 1 or S[i] == -1 for all i         ###
-###   len(A) == len(S) == INPUT_LEN             ###
-def getResidueSeq(A,S):
-    return abs(sum(A[i] * S[i] for i in xrange(INPUT_LEN)))
-    
-### Return residue given array A and partition P ###
-###   A[i] is in partition group P[i]            ###
-### Invariants:                                  ###
-###   len(A) == len(P) == INPUT_LEN              ###
-###   0 <= P[i] <= INPUT_LEN for all i           ###
-def getResiduePP(A,P):
-    return kk(prePartition(A,P))
-
-### Repeated Random with solution sequences ###
+### Repeated Random with solution sequences
+@experiment
 def rrSeq(A):
     minResidue = maxint
-    for _ in xrange(NUM_TRIALS):
+    for _ in xrange(NUM_ITERS):
         S = genSolutionSeq()
         residue = getResidueSeq(A, S)
         minResidue = min(residue, minResidue)
     return minResidue
 
-### Repeated Random with prepartitions ###
+### Repeated Random with prepartitions
+@experiment
 def rrPrePart(A):
     minResidue = maxint
-    for _ in xrange(NUM_TRIALS):
+    for _ in xrange(NUM_ITERS):
         P = genPrepartition()
         residue = getResiduePP(A, P)
         minResidue = min(residue, minResidue)
     return minResidue
 
-### Hill Climbing with solution sequences ###
+### Hill Climbing with solution sequences
+@experiment
 def hcSeq(A):
     S = genSolutionSeq()
     minResidue = getResidueSeq(A, S)
-    for _ in xrange(NUM_TRIALS):
+    for _ in xrange(NUM_ITERS):
         SNeighbor = genSeqNeighbor(S[:])
         residue = getResidueSeq(A, SNeighbor)
         if residue < minResidue:
@@ -123,11 +170,12 @@ def hcSeq(A):
             minResidue = residue
     return minResidue
 
-### Hill Climbing with prepartitions ###
+### Hill Climbing with prepartitions
+@experiment
 def hcPrePart(A):
     P = genPrepartition()
     minResidue = getResiduePP(A, P)
-    for _ in xrange(NUM_TRIALS):
+    for _ in xrange(NUM_ITERS):
         PNeighbor = genPrePartNeighbor(P[:])
         residue = getResiduePP(A, PNeighbor)
         if residue < minResidue:
@@ -135,15 +183,12 @@ def hcPrePart(A):
             minResidue = residue
     return minResidue
 
-### Cooling schedule function for simulated annealing ###
-def saCooling(i):
-    return (10**8) * (0.8**(i // 300))
-
-### Simulated Annealing with solution sequences ###
+### Simulated Annealing with solution sequences
+@experiment
 def saSeq(A):
     S = genSolutionSeq()
-    minResidue = getResidueSeq(A, S)
-    for i in xrange(NUM_TRIALS):
+    minResidue = maxint
+    for i in xrange(NUM_ITERS):
         SNeighbor = genSeqNeighbor(S[:])
         residueS = getResidueSeq(A, S)
         residueSNeighbor = getResidueSeq(A, SNeighbor)
@@ -158,11 +203,12 @@ def saSeq(A):
         minResidue = min(residueS, minResidue)
     return minResidue
 
-### Simulated Annealing with prepartitions ###
+### Simulated Annealing with prepartitions
+@experiment
 def saPrePart(A):
     P = genPrepartition()
-    minResidue = getResiduePP(A, P)
-    for i in xrange(NUM_TRIALS):
+    minResidue = maxint
+    for i in xrange(NUM_ITERS):
         PNeighbor = genPrePartNeighbor(P[:])
         residueP = getResiduePP(A, P)
         residuePNeighbor = getResiduePP(A, PNeighbor)
@@ -177,21 +223,24 @@ def saPrePart(A):
         minResidue = min(residueP, minResidue)
     return minResidue
 
+####################
+###     MAIN     ###
+####################
+
 def main():
     A = []
     if len(argv) < 2:
-        ## print "Usage: python ./kk.py inputfile"
-        ## exit(1)
-        A = getArray()
+        A = genRandArray()
     else:
         A = getArray(argv[1])
-    print kk(A[:])
-    print rrSeq(A[:])
-    print rrPrePart(A[:])
-    print hcSeq(A[:])
-    print hcPrePart(A[:])
-    print saSeq(A[:])
-    print saPrePart(A[:])
+    ## cannot decorate KK since it's used by other functions
+    print "KK:  " + str(experiment(kk)(A[:]))
+    print "RRS: " + str(rrSeq(A[:]))
+    print "RRP: " + str(rrPrePart(A[:]))
+    print "HCS: " + str(hcSeq(A[:]))
+    print "HCP: " + str(hcPrePart(A[:]))
+    print "SAS: " + str(saSeq(A[:]))
+    print "SAP: " + str(saPrePart(A[:]))
     
 main()
 
